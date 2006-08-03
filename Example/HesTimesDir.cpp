7a5ce56df716d16f18a774caa1fd07a8eb8a2c7c
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2006-08-03 06:36:08 +0000

    Batch edit conversion from GPL to CPL License
    git-svn-id: https://projects.coin-or.org/svn/CppAD/branches/copyright@475 0e562018-b8fb-0310-a933-ecf073c7c197

--- Example/HesTimesDir.cpp
+++ Example/HesTimesDir.cpp
@@ -1,20 +1,10 @@
-/* -----------------------------------------------------------------------
+/* --------------------------------------------------------------------------
 CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-06 Bradley M. Bell
 
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
------------------------------------------------------------------------- */
+This program is free software; you can use it under the terms of the 
+	         Common Public License Version 1.0.
+You should have received a copy of the this license along with this program.
+-------------------------------------------------------------------------- */
 
 /*
 $begin HesTimesDir.cpp$$
