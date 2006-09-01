7b5b86b9c9494a1c30a8bcb1d01114072a87ea62
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2006-09-01 15:23:45 +0000

    Change licensing message to the following text:
    CppAD is distributed under multiple licenses. This distribution is under
    the terms of the
                        Common Public License Version 1.0.
    
    A copy of this license is included in the COPYING file of this distribution.
    
    
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/branches/filename@510 0e562018-b8fb-0310-a933-ecf073c7c197

--- BatchCommit.sh
+++ BatchCommit.sh
@@ -2,26 +2,29 @@
 # -----------------------------------------------------------------------------
 # CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-06 Bradley M. Bell
 #
-# This program is free software; you can use it under the terms of the 
-#	         Common Public License Version 1.0.
-# You should have received a copy of the this license along with this program.
+# CppAD is distributed under multiple licenses. This distribution is under
+# the terms of the 
+#                     Common Public License Version 1.0.
+#
+# A copy of this license is included in the COPYING file of this distribution.
 # ----------------------------------------------------------------------
-log_entry="Change CppAD/local/*_.hpp (temporary file name) to 
-CppAD/local/*.hpp (desired file name).
+log_entry="Change licensing message to the following text:
+
+CppAD is distributed under multiple licenses. This distribution is under
+the terms of the
+                    Common Public License Version 1.0.
+
+A copy of this license is included in the COPYING file of this distribution.
 "
-list1=`ls CppAD/local/*.hpp | sed -e 's|\.hpp|_.hpp|'` 
-list2=`ls CppAD/local/*.hpp` 
-list3=`ls CppAD/*.h | sed -e '/config.h/d'` 
-list4=`ls omh/*.omh` 
-change_list="
-$list1
-$list2
-$list3
-$list4
-Doc.omh
-Dev.omh
-Makefile.am
-"
+list=`find . \
+	\( -name '*.am'  \) -or \
+	\( -name '*.ac'  \) -or \
+	\( -name '*.sh' \) -or \
+	\( -name '*.cpp' \) -or \
+	\( -name '*.h'   \) -or \
+	\( -name '*.hpp' \) -or \
+	\( -name '*.omh' \) | \
+	sed -e '/ChangeCopy\.sh/d' -e '/config\.h/d'`
 echo "$log_entry" > SvnCommit.log
-echo "svn commit --username bradbell --file SvnCommit.log $change_list"
-svn commit --username bradbell --file SvnCommit.log $change_list
+echo "svn commit --username bradbell --file SvnCommit.log $list"
+svn commit --username bradbell --file SvnCommit.log $list
--- CppAD/local/user_ad.hpp
+++ CppAD/local/user_ad.hpp
@@ -4,9 +4,11 @@
 /* --------------------------------------------------------------------------
 CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-06 Bradley M. Bell
 
-This program is free software; you can use it under the terms of the 
-	         Common Public License Version 1.0.
-You should have received a copy of the this license along with this program.
+CppAD is distributed under multiple licenses. This distribution is under
+the terms of the 
+                    Common Public License Version 1.0.
+
+A copy of this license is included in the COPYING file of this distribution.
 -------------------------------------------------------------------------- */
 /*
 ---------------------------------------------------------------------------
