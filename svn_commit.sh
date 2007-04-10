ff3484de86cec13d18cf6e9f0599e0723673f0ef
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2007-04-10 12:30:28 +0000

    branches/intro: Add example for exp_2_rev2.
    svn_commit.sh: file that made this commit.
    whats_new_07.omh: user's view of the changes.
    exp_2_cppad.cpp: added second order calculations.
    
    
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/branches/intro@918 0e562018-b8fb-0310-a933-ecf073c7c197

--- svn_commit.sh
+++ svn_commit.sh
@@ -46,15 +46,9 @@ log_entry="Add example for exp_2_rev2.
 
 svn_commit.sh: file that made this commit.
 whats_new_07.omh: user's view of the changes.
-exp_eps_rev1.cpp: fix f2 to f1 in comment.
-exp_2_rev1.cpp: fix f2 to f1 in comment.
-exp_2.omh: add second order reverse sweep section exp_2_rev2.
-main.cpp: add exp_2_rev.
-exp_apx.vcproj: add exp_2_rev.
-makefile.am: add exp_2_rev.
+exp_2_cppad.cpp: added second order calculations.
 " 
 add_list="
-	introduction/exp_apx/exp_2_rev2.cpp
 "
 delete_list="
 "
@@ -64,12 +58,7 @@ move_list="
 change_list="
 	svn_commit.sh
 	omh/whats_new_07.omh
-	introduction/exp_apx/exp_eps_rev1.cpp
-	introduction/exp_apx/exp_2_rev1.cpp
-	introduction/exp_apx/exp_2.omh
-	introduction/exp_apx/main.cpp
-	introduction/exp_apx/exp_apx.vcproj
-	introduction/exp_apx/makefile.am
+	introduction/exp_apx/exp_2_cppad.cpp
 "
 #
 copy_branch="" 
