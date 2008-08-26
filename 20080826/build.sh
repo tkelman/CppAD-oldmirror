6f08c736882664b7e586f1e36c5f4203e25609ab
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2008-08-26 13:57:29 +0000

    stable/20080826: Changes automatically generated by trunk/new_stable.sh.
    configure: set fixed version number.
    svn_status.sh: remove checks for moving version number.
    AUTHORS: set date corresponding to version.
    configure.ac: set fixed version number.
    build.sh: set fixed version number.
    omh/install_windows.omh.in: set link to download directory.
    omh/install_unix.omh.in: set link to download directory.
    config.h: set fixed version number.
    
    
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/stable@1263 0e562018-b8fb-0310-a933-ecf073c7c197

--- 20080826/build.sh
+++ 20080826/build.sh
@@ -52,8 +52,8 @@ then
 	#
 	# Today's date in yy-mm-dd decimal digit format where 
 	# yy is year in century, mm is month in year, dd is day in month.
-	yyyymmdd=`date +%G%m%d`
-	yyyy_mm_dd=`date +%G-%m-%d`
+	yyyymmdd="20080826"
+	yyyy_mm_dd="2008-08-26"
 	#
 	# automatically change version for certain files
 	sed < AUTHORS > AUTHORS.$$ \
