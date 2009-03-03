7fa37cb5548cba3642537e1190035a38e4148145
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2009-03-03 14:40:25 +0000

    stable/20090303: Changes generated by new_stable.sh
    git-svn-id: https://projects.coin-or.org/svn/CppAD/stable/20090303@1351 0e562018-b8fb-0310-a933-ecf073c7c197

--- build.sh
+++ build.sh
@@ -52,8 +52,8 @@ then
 	#
 	# Today's date in yy-mm-dd decimal digit format where 
 	# yy is year in century, mm is month in year, dd is day in month.
-	yyyymmdd=`date +%G%m%d`
-	yyyy_mm_dd=`date +%G-%m-%d`
+	yyyymmdd="20090303.0"
+	yyyy_mm_dd="2009-03-03"
 	#
 	# automatically change version for certain files
 	# (the [.0-9]* is for using build.sh in CppAD/stable/* directories)
