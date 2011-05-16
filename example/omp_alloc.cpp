d54d5297a94cda26475012650e5c908df6a41f96
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2011-05-16 19:25:16 +0000

    omp_alloc.hpp: check that return_memory argument is in use. omp_alloc.cpp: include multiple inuse allocations in example.
    git-svn-id: https://projects.coin-or.org/svn/CppAD/branches/omp_alloc@1927 0e562018-b8fb-0310-a933-ecf073c7c197

--- example/omp_alloc.cpp
+++ example/omp_alloc.cpp
@@ -31,6 +31,7 @@ $end
 */
 // BEGIN PROGRAM
 # include <cppad/omp_alloc.hpp>
+# include <cppad/vector.hpp>
 
 bool omp_alloc(void)
 {	bool ok = true;
@@ -56,32 +57,38 @@ bool omp_alloc(void)
 	// repeatedly allocate enough memory for at least two size_t values.
 	size_t min_size_t = 2;
 	size_t min_bytes  = min_size_t * sizeof(size_t);
-	size_t n_repeat   = 100;
-	size_t num_bytes, i, j;
-	for(i = 0; i < n_repeat; i++)
-	{	// allocate enough memory for a size_t object
-		void *v_ptr = omp_alloc::get_memory(min_bytes, num_bytes);
-		size_t* ptr = reinterpret_cast<size_t*>(v_ptr);
-		// determine the number of size_t valuse we have obtained
-		size_t  num_size_t = num_bytes / sizeof(size_t);
-		ok                &= min_size_t <= num_size_t;
-		// use placement new to call the size_t copy constructor
-		for(j = 0; j < num_size_t; j++)
-			new(ptr + j) size_t(i + j);
-		// check that the constructor worked
-		for(j = 0; j < num_size_t; j++)
-			ok &= ptr[j] == (i+j);
-		// check that num_bytes are inuse (none are available now)
-		ok &= omp_alloc::inuse(thread) == num_bytes;
+	size_t n_outter   = 10;
+	size_t n_inner    = 5;
+	size_t num_bytes, i, j, k;
+	for(i = 0; i < n_outter; i++)
+	{	CppAD::vector<void*> v_ptr(n_inner);
+		for( j = 0; j < n_inner; j++)
+		{	// allocate enough memory for min_size_t size_t objects
+			v_ptr[j]    = omp_alloc::get_memory(min_bytes, num_bytes);
+			size_t* ptr = reinterpret_cast<size_t*>(v_ptr[j]);
+			// determine the number of size_t valuse we have obtained
+			size_t  num_size_t = num_bytes / sizeof(size_t);
+			ok                &= min_size_t <= num_size_t;
+			// use placement new to call the size_t copy constructor
+			for(k = 0; k < num_size_t; k++)
+				new(ptr + k) size_t(i + j + k);
+			// check that the constructor worked
+			for(k = 0; k < num_size_t; k++)
+				ok &= ptr[k] == (i + j + k);
+		}
+		// check that n_inner * num_bytes are inuse and none are available
+		ok &= omp_alloc::inuse(thread) == n_inner * num_bytes;
 		ok &= omp_alloc::available(thread) == 0;
 		// return the memrory to omp_alloc
-		omp_alloc::return_memory(v_ptr);
-		// check that now num_bytes are available 
-		ok &= omp_alloc::available(thread) == num_bytes;
+		for(j = 0; j < n_inner; j++)
+			omp_alloc::return_memory(v_ptr[j]);
+		// check that now n_inner * num_bytes are now available
+		// and none are in use
+		ok &= omp_alloc::inuse(thread) == 0;
+		ok &= omp_alloc::available(thread) == n_inner * num_bytes;
 	}
 	// return all the available memory to the system
 	omp_alloc::free_available(thread);
-	ok &= omp_alloc::inuse(thread) == 0;
 	ok &= omp_alloc::available(thread) == 0;
 	
 	return ok;
