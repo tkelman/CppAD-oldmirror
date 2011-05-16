7c31bda705f61cfdfb268032a0f13daeb8f4ff37
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2011-05-16 14:52:55 +0000

    Change get_memory so it returns the actual number of bytes allocated, not just the requested number of bytes.
    omp_alloc.hpp: implement new feature.
    omp_alloc.cpp: test new feature.
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/branches/omp_alloc@1924 0e562018-b8fb-0310-a933-ecf073c7c197

--- example/omp_alloc.cpp
+++ example/omp_alloc.cpp
@@ -39,9 +39,6 @@ bool omp_alloc(void)
 	// check not in parallel mode
 	ok &= omp_alloc::in_parallel() == false;
 
-	// check that thread number is zero when in sequential mode
-	ok &= omp_alloc::get_thread_num() == 0;
-
 	// check initial memory values
 	size_t thread;
 	for(thread = 0; thread < CPPAD_MAX_NUM_THREADS; thread++)
@@ -51,21 +48,38 @@ bool omp_alloc(void)
 		ok &= omp_alloc::available(thread) == 0;
 	}
 
-	// repeatedly allocate the same amount of memory
-	size_t num_bytes = sizeof(size_t);
-	size_t i         = 100;
-	while(i--)
+	// repeatedly allocate enough memory for at least two size_t values.
+	thread            = omp_alloc::get_thread_num();
+	size_t min_size_t = 2;
+	size_t min_bytes  = min_size_t * sizeof(size_t);
+	size_t n_repeat   = 100;
+	size_t num_bytes, i, j;
+	for(i = 0; i < n_repeat; i++)
 	{	// allocate enough memory for a size_t object
-		void *v_ptr = omp_alloc::get_memory(num_bytes);
+		void *v_ptr = omp_alloc::get_memory(min_bytes, num_bytes);
 		size_t* ptr = reinterpret_cast<size_t*>(v_ptr);
+		// determine the number of size_t valuse we have obtained
+		size_t  num_size_t = num_bytes / sizeof(size_t);
+		ok                &= min_size_t <= num_size_t;
 		// use placement new to call the size_t constructor
-		new(ptr) size_t(i + 1);
+		for(j = 0; j < num_size_t; j++)
+			new(ptr + j) size_t(i + j);
 		// check that the constructor worked
-		ok &= (*ptr) == (i+1);
+		for(j = 0; j < num_size_t; j++)
+			ok &= ptr[j] == (i+j);
+		// check that num_bytes are inuse (none are available now)
+		ok &= omp_alloc::inuse(thread) == num_bytes;
+		ok &= omp_alloc::available(thread) == 0;
 		// return the memrory to omp_alloc
 		omp_alloc::return_memory(v_ptr);
+		// check that now num_bytes are available 
+		ok &= omp_alloc::available(thread) == num_bytes;
 	}
-
+	// return all the available memory to the system
+	omp_alloc::free_available(thread);
+	ok &= omp_alloc::inuse(thread) == 0;
+	ok &= omp_alloc::available(thread) == 0;
+	
 	return ok;
 }
 
