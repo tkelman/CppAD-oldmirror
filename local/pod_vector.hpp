8fe799caf7f34a9d0b05973d1e192ce75dbf172b
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2011-05-19 23:12:28 +0000

    Convert recorder class to use omp_alloc for its memory allocation.
    add_copyright.sh: also add svn Id to beginning of file.
    makefile.am: add pod_vector.hpp to the distribution.
    makefile.in: changes from corresponding makefile.am.
    player.hpp: use pod_vector object to access recorder information.
    pod_vector.hpp: vectors of Plain Old Data.
    recorder.hpp: simplify and omp_alloc allocation using pod_vector.
    compare.sh: compares speed of new and current verison of CppAD.
    makefile.am: copy compare.sh to work directory.
    makefile.in: changes from corresponding makefile.am.
    main.cpp: fix memory leak check (when NDEBUG is not defined).
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/branches/omp_alloc@1936 0e562018-b8fb-0310-a933-ecf073c7c197

---
+++ local/pod_vector.hpp
@@ -0,0 +1,148 @@
+/* $Id$ */
+# ifndef CPPAD_POD_VECTOR_INCLUDED
+# define CPPAD_POD_VECTOR_INCLUDED
+
+/* --------------------------------------------------------------------------
+CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-11 Bradley M. Bell
+
+CppAD is distributed under multiple licenses. This distribution is under
+the terms of the 
+                    Common Public License Version 1.0.
+
+A copy of this license is included in the COPYING file of this distribution.
+Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
+-------------------------------------------------------------------------- */
+
+# include <cppad/local/cppad_assert.hpp>
+# include <cppad/omp_alloc.hpp>
+
+CPPAD_BEGIN_NAMESPACE
+/*!
+\file pod_vector.hpp
+File used to define pod_vector class
+*/
+
+// ---------------------------------------------------------------------------
+/*!
+Vector class with Type elements where Type is Plain Old Data.
+*/
+template <class Type>
+class pod_vector {
+private:
+	/// maximum number of Type elements current allocation can hold
+	size_t capacity_;
+	/// number of elements currently in this vector
+	size_t length_;
+	/// pointer to the first type elements 
+	/// (not defined and should not be used when capacity_ = 0)
+	Type   *data_;
+public:
+	/// Default constructor: sets capacity and length to zero
+	/// (data is also set to zero to avoid compiler warnings).
+	inline pod_vector(void) 
+	: capacity_(0), length_(0), data_(0)
+	{ }
+	// ----------------------------------------------------------------------
+	/// Destructor: returns allocated memory to \c omp_alloc; see \c extend.
+	/// The element destructors are not called because this is Plain Old Data.
+	~pod_vector(void)
+	{	if( capacity_ > 0 )
+		{	void* v_ptr = reinterpret_cast<void*>( data_ );
+			omp_alloc::return_memory(v_ptr); 
+		}
+	}
+	// ----------------------------------------------------------------------
+	/// current number of elements in this vector.
+	inline size_t size(void) const
+	{	return length_; }
+	/// current capacity (amount of allocated storage) for this vector.
+	inline size_t capacity(void) const
+	{	return capacity_; }
+	
+	// ----------------------------------------------------------------------
+	/*!
+	Increase the number of elements the end of this vector. 
+
+	\param n
+	is the number of elements to add to this vector.
+
+	\return
+	is the number  of elements in the vector before it was extended.
+
+	- The new elements are not initialized; i.e., their constructor 
+	is not called (becasue they are Plain Old Data; i.e. pod).
+
+	- This is the only routine that allocates memory for \c pod_vector.
+	and it uses omp_alloc for this allocation, hence this determines
+	which thread corresponds to this vector (when in parallel mode).
+	*/
+	inline size_t extend(size_t n)
+	{	size_t old_length = length_;
+		length_          += n;
+		// check if we can use current memory
+		if( capacity_ >= length_ )
+			return old_length;
+
+		// save old memory pointer
+		Type* old_data      = data_;
+
+		// get new memory and set capacity
+		size_t length_bytes = length_ * sizeof(Type);
+		size_t capacity_bytes;
+		void* v_ptr = omp_alloc::get_memory(length_bytes, capacity_bytes);
+		capacity_   = capacity_bytes / sizeof(Type);
+		data_       = reinterpret_cast<Type*>(v_ptr);
+		CPPAD_ASSERT_UNKNOWN( length_ <= capacity_ );
+
+		// copy old data to new data
+		size_t i;
+		for(i = 0; i < old_length; i++)
+			data_[i] = old_data[i];
+
+		// return old memory to available pool
+		if( old_length > 0 )
+		{	v_ptr = reinterpret_cast<void*>( old_data );
+			omp_alloc::return_memory(v_ptr); 
+		}
+
+		// return value for extend is the old length
+		return old_length;
+	}
+	// ----------------------------------------------------------------------
+	/// non-constant element access; i.e., we can change this element value
+	Type& operator[](
+		/// element index, must be less than length
+		size_t i
+	)
+	{	CPPAD_ASSERT_UNKNOWN( i < length_ );
+		return data_[i]; 
+	}
+	// ----------------------------------------------------------------------
+	/// constant element access; i.e., we cannot change this element value
+	const Type& operator[](
+		/// element index, must be less than length
+		size_t i
+	) const
+	{	CPPAD_ASSERT_UNKNOWN( i < length_ );
+		return data_[i]; 
+	}
+	// ----------------------------------------------------------------------
+	/*!
+ 	Erase all the information in this vector.
+
+	- size and capacity of this vector are set to zero.
+
+	- the memory allocated for this vector is returned to \c omp_alloc.
+	*/
+	void erase(void)
+	{	if( capacity_ > 0 )
+		{	void* v_ptr = reinterpret_cast<void*>( data_ );
+			omp_alloc::return_memory(v_ptr); 
+		}
+		capacity_ = length_ = 0;
+		return;
+	}	
+};
+
+CPPAD_END_NAMESPACE
+# endif
