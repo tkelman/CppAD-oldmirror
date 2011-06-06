b083451a99b422708430816cd31755317c1fc923
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2011-06-06 15:10:03 +0000

    Make type used to store OpCode a define (so can use less memory).
    speed_compare.sh: include available in comparison.
    memory_leak.hpp: minor improvement to documentation.
    main.cpp: include available in comparison.
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/branches/memory@1964 0e562018-b8fb-0310-a933-ecf073c7c197

--- local/undef.hpp
+++ local/undef.hpp
@@ -3,7 +3,7 @@
 # define CPPAD_UNDEF_INCLUDED
 
 /* --------------------------------------------------------------------------
-CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-07 Bradley M. Bell
+CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-11 Bradley M. Bell
 
 CppAD is distributed under multiple licenses. This distribution is under
 the terms of the 
@@ -15,6 +15,7 @@ Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
 
 // Undo definitions in define.hpp 
 
+# undef CPPAD_OP_CODE_TYPE
 # undef CPPAD_NULL
 # undef CPPAD_MAX_NUM_THREADS
 # undef CPPAD_FOLD_ASSIGNMENT_OPERATOR
