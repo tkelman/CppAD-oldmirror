7c7de0cab494a7813e122dfc92fa5f95342e53a5
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2011-06-08 03:40:23 +0000

    pod_vector.hpp: define is_pod<OP_CODE_TYPE> as true. define.hpp: Change OP_CODE_TYPE from size_t to char. undef.hpp: include all define.hpp symbols (except NAMESPACE ones).
    git-svn-id: https://projects.coin-or.org/svn/CppAD/branches/memory@1965 0e562018-b8fb-0310-a933-ecf073c7c197

--- local/undef.hpp
+++ local/undef.hpp
@@ -14,13 +14,13 @@ Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
 -------------------------------------------------------------------------- */
 
 // Undo definitions in define.hpp 
-
 # undef CPPAD_OP_CODE_TYPE
+# undef CPPAD_USE_FORWARD0SWEEP
+# undef CPPAD_INLINE
 # undef CPPAD_NULL
 # undef CPPAD_MAX_NUM_THREADS
 # undef CPPAD_FOLD_ASSIGNMENT_OPERATOR
 # undef CPPAD_FOLD_AD_VALUED_BINARY_OPERATOR
 # undef CPPAD_FOLD_BOOL_VALUED_BINARY_OPERATOR
-
 
 # endif
