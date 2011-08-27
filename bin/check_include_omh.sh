d555d027939660bad2ad95a6f1939bf4a3dda5af
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2011-08-27 12:40:09 +0000

    Improve wrong working directory detection.
    add_copyright.sh: detect non-existent file, fix name in error messages.
    
    git-svn-id: https://projects.coin-or.org/svn/CppAD/branches/pthread@2076 0e562018-b8fb-0310-a933-ecf073c7c197

--- bin/check_include_omh.sh
+++ bin/check_include_omh.sh
@@ -10,7 +10,7 @@
 # A copy of this license is included in the COPYING file of this distribution.
 # Please visit http://www.coin-or.org/CppAD/ for information on other licenses.
 # -----------------------------------------------------------------------------
-if [ $0 != "bin/check_include_omh.sh" ]
+if [ ! -e "bin/check_include_omh.sh" ]
 then
 	echo "bin/check_include_omh.sh: must be executed from its parent directory"
 	exit 1
