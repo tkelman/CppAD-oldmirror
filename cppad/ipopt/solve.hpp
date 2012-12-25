c0b284ac38dcf5432e7ca553fc51db45b29f39ea
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2012-12-25 23:04:39 +0000

    first version of solve_nlp.hpp that compiles
    git-svn-id: https://projects.coin-or.org/svn/CppAD/branches/ipopt_solve@2641 0e562018-b8fb-0310-a933-ecf073c7c197

--- cppad/ipopt/solve.hpp
+++ cppad/ipopt/solve.hpp
@@ -318,56 +318,10 @@ $end
 -------------------------------------------------------------------------------
 */
 # include <cppad_ipopt_nlp.hpp>
+# include <cppad/ipopt/solve_nlp.hpp>
 
 CPPAD_BEGIN_NAMESPACE
 namespace ipopt {
-/*!
-Class that contains information about solve problem result
-
-\tparam Dvector
-a simple vector with elements of type double
-*/
-template <class Dvector>
-class solve_result 
-{	
-public:
-	/// possible values for the result status
-	enum status_type {
-		not_defined,
-		success,
-		maxiter_exceeded,
-		stop_at_tiny_step,
-		stop_at_acceptable_point,
-		local_infeasibility,
-		user_requested_stop,
-		feasible_point_found,
-		diverging_iterates,
-		restoration_failure,
-		error_in_step_computation,
-		invalid_number_detected,
-		too_few_degrees_of_freedom,
-		internal_error,
-		unknown
-	};
-
-	/// possible values for solution status
-	status_type status;
-	/// the approximation solution
-	Dvector x;
-	/// Lagrange multipliers corresponding to lower bounds on x
-	Dvector zl;
-	/// Lagrange multipliers corresponding to upper bounds on x
-	Dvector zu;
-	/// value of g(x)
-	Dvector g;
-	/// Lagrange multipliers correspondiing constraints on g(x)
-	Dvector lambda;
-	/// value of f(x)
-	double obj_value;
-	/// constructor initializes solution status as not yet defined
-	solve_result(void)
-	{	status = not_defined; }
-};
 
 /*!
 FG_info class used by cppad_ipopt_nlp (used for temporary conversion).
@@ -458,7 +412,7 @@ void solve(
 	const Dvector&                       gu        , 
 	FG_eval&                             fg_eval   , 
 	const char*                          options   ,
-	solve_result<Dvector>&         solution  )
+	ipopt::solve_result<Dvector>&               solution  )
 { 	bool ok = true;
 
 	typedef typename FG_eval::ADvector ADvector;
