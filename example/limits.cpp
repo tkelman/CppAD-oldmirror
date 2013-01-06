9ec1cc328c05b9080fc86b291efaec659926d9c8
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2013-01-06 17:51:41 +0000

    Fix problem where test_more/epsilon.cpp and example/limits.cpp were failing while testing for Fedora devel branch by copying solution from trunk.
    git-svn-id: https://projects.coin-or.org/svn/CppAD/stable/20130000@2723 0e562018-b8fb-0310-a933-ecf073c7c197

--- example/limits.cpp
+++ example/limits.cpp
@@ -1,6 +1,6 @@
 /* $Id$ */
 /* --------------------------------------------------------------------------
-CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-12 Bradley M. Bell
+CppAD: C++ Algorithmic Differentiation: Copyright (C) 2003-13 Bradley M. Bell
 
 CppAD is distributed under multiple licenses. This distribution is under
 the terms of the 
@@ -31,8 +31,8 @@ $end
 # ifdef _MSC_VER
 // Supress Microsoft compiler warning about possible loss of precision,
 // in the constructors (when converting to std::complex<float>)
-//	Type one = 1
-//	Type two = 2
+//	Float one = 1
+//	Float two = 2
 // 1 and 2 are small enough so no loss of precision when converting to float.
 # pragma warning(disable:4244)
 # endif
@@ -41,128 +41,53 @@ $end
 # include <complex>
 
 namespace {
-	template <class Type>
-	Type add_one(const Type& value)
-	{	return( Type(1) + value ); }
+	typedef CppAD::AD<double> Float;
+	//
 	// -----------------------------------------------------------------
-	template <class Type>
 	bool check_epsilon(void)
 	{	bool ok    = true;
-		Type eps   = CppAD::numeric_limits<Type>::epsilon();
-		Type one   = 1;
-		Type two   = 2;
-		Type eps2  = eps / two; 
-		Type check = add_one(eps);
-		ok        &= one !=  check;
-		check      = add_one(eps2);
-		ok        &= one == check;
+		Float eps   = CppAD::numeric_limits<Float>::epsilon();
+		Float eps2  = eps / 2.0; 
+		Float check = 1.0 + eps;
+		ok         &= 1.0 !=  check;
+		check       = 1.0 + eps2;
+		ok         &= 1.0 == check;
 		return ok;
 	}
 	// -----------------------------------------------------------------
-	template <class Type>
 	bool check_min(void)
-	{	bool ok    = true;
-		Type min   = CppAD::numeric_limits<Type>::min();
-		Type eps   = CppAD::numeric_limits<Type>::epsilon();
-		Type one   = 1;
-		Type three = 3;
-		Type tmp   = (min * eps) / three;
-		Type match = (tmp * three) / eps;
-		//        &= abs(match/min - one) > three * eps
-		ok        &= CppAD::abs_geq(match/min - one, three * eps);
+	{	bool ok     = true;
+		Float min   = CppAD::numeric_limits<Float>::min();
+		Float eps   = CppAD::numeric_limits<Float>::epsilon();
 		//
-		tmp        = (min / eps) * three;
-		match      = (tmp / three) * eps;
-		//        &= abs(match / min - one) < three * eps
-		ok        &= CppAD::abs_geq(three * eps, match / min - one);
+		Float match = (min / 100. ) * 100.;
+		ok         &= abs(match/min - 1.0) > 3.0 * eps;
+		//
+		match       = (min * 100.) / (100. * (1.0 - eps));
+		ok         &= abs(match / min - 1.0) < 3.0 * eps;
 		return ok;
 	}
 	// -----------------------------------------------------------------
-	template <class Type>
 	bool check_max(void)
-	{	bool ok    = true;
-		Type max   = CppAD::numeric_limits<Type>::max();
-		Type eps   = CppAD::numeric_limits<Type>::epsilon();
-		Type one   = 1;
-		Type three = 3;
-		Type tmp   = max * ( one / eps);
-		Type match = tmp * eps;
-		ok        &= CppAD::abs(match / max - one) > three * eps;
-		return ok;
-	}
-	//
-	template <class Type>
-	bool check_max_complex(void)
-	{	typedef std::complex<Type> Complex;
-		bool ok    = true;
-		Complex c_max   = CppAD::numeric_limits<Complex>::max();
-		Complex c_eps   = CppAD::numeric_limits<Type>::epsilon();
-		ok &= c_eps.imag() == Type(0);
-		ok &= c_max.imag() == Type(0);
-		Type max   = c_max.real();
-		Type eps   = c_eps.real();
-		Type one   = 1;
-		Type three = 3;
-		Type tmp   = max * ( one / eps);
-		Type match = tmp * eps;
-		ok        &= CppAD::abs(match / max - one) > three * eps;
-		return ok;
-	}
-	//
-	template <class Base>
-	bool check_max_ad_complex()
-	{	using CppAD::AD;
-		bool ok    = true;
-		AD<Base> c_max   = CppAD::numeric_limits< AD<Base> >::max();
-		ok &= Value(c_max).imag() == Base(0);
-		ok &= Value(c_max).real() == CppAD::numeric_limits<Base>::max();
-
+	{	bool ok     = true;
+		Float max   = CppAD::numeric_limits<Float>::max();
+		Float eps   = CppAD::numeric_limits<Float>::epsilon();
+		//
+		Float match = (max * 100.) / 100.;
+		ok         &= abs(match / max - 1.0) > 3.0 * eps;
+		//
+		match       = (max / 100. ) * (100. * (1.0 - eps));
+		ok         &= abs(match/max - 1.0) < 3.0 * eps;
 		return ok;
 	}
 }
 
 bool limits(void)
 {	bool ok = true;
-	using CppAD::AD;
 
-	// -------------------------------------------------------------------
-	// epsilon for Base types defined by CppAD
-	ok &= check_epsilon<float>();
-	ok &= check_epsilon<double>();
-	ok &= check_epsilon< std::complex<float> >();
-	ok &= check_epsilon< std::complex<double> >();
-
-	// epsilon for some AD types. 
-	ok &= check_epsilon< AD<float> >();
-	ok &= check_epsilon< AD<double> >();
-	ok &= check_epsilon<  AD<std::complex<float> > >();
-	ok &= check_epsilon<  AD<std::complex<double> > >();
-
-	// -------------------------------------------------------------------
-	// min for Base types defined by CppAD
-	ok &= check_min<float>();
-	ok &= check_min<double>();
-	ok &= check_min< std::complex<float> >();
-	ok &= check_min< std::complex<double> >();
-
-	// min for some AD types. 
-	ok &= check_min< AD<float> >();
-	ok &= check_min< AD<double> >();
-	ok &= check_min<  AD<std::complex<float> > >();
-	ok &= check_min<  AD<std::complex<double> > >();
-
-	// -------------------------------------------------------------------
-	// max for Base types defined by CppAD
-	ok &= check_max<float>();
-	ok &= check_max<double>();
-	ok &= check_max_complex<float>();
-	ok &= check_max_complex<double>();
-
-	// max for some AD types. 
-	ok &= check_max< AD<float> >();
-	ok &= check_max< AD<double> >();
-	ok &= check_max_ad_complex< std::complex<float> >();
-	ok &= check_max_ad_complex< std::complex<double> >();
+	ok &= check_epsilon();
+	ok &= check_max();
+	ok &= check_min();
 
 	return ok;
 }
