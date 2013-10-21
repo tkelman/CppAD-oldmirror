8110d891bac7a6a8812e4575df312059fa9fcb49
bradbell <bradbell@0e562018-b8fb-0310-a933-ecf073c7c197>
2013-10-21 15:50:46 +0000

    Implement and test CExpOp optimization with reverse sparse Hessian calculation.
    git-svn-id: https://projects.coin-or.org/svn/CppAD/branches/opt_cond_exp@2988 0e562018-b8fb-0310-a933-ecf073c7c197

--- local/rev_hes_sweep.hpp
+++ local/rev_hes_sweep.hpp
@@ -277,6 +277,14 @@ void RevHesSweep(
 			break;
 			// -------------------------------------------------
 
+			case CSkipOp:
+			// CSkipOp has a variable number of arguments and
+			// next_reverse thinks it one has one argument.
+			// We must inform next_reverse of this special case.
+			play->reverse_cskip(op, arg, i_op, i_var);
+			break;
+			// -------------------------------------------------
+
 			case CSumOp:
 			// CSumOp has a variable number of arguments and
 			// next_reverse thinks it one has one argument.
